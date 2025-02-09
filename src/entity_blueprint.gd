6b91e7f3b8a8621d5da72b824b01aecfa0d57ca0
norsker <emilgraaenorsker@gmail.com>
2025-02-09 10:53:20 +0100

    refactor(blueprint): Simplify EntityBlueprint with component array - Remove individual component blueprints - Add component array with name and properties - Add helper methods for component management

--- src/entity_blueprint.gd
+++ src/entity_blueprint.gd
@@ -2,19 +2,29 @@
 extends Resource
 class_name EntityBlueprint
 
-@export var name: String = ""
-@export var texture: AtlasTexture
-@export var color: Color = Color.WHITE
-@export var is_blocking_movment: bool = false
+@export var entity_name: String = ""
+@export var blocks_movement: bool = false
 @export var type: Entity.EntityType = Entity.EntityType.ACTOR
-@export var ai_type: Entity.AIType = Entity.AIType.NONE
+@export var key: String = ""
 
 @export_category("Components")
-@export var combat_blueprint: CombatComponentBlueprint
-@export var terrain_blueprint: TerrainComponentBlueprint
-@export var ai_blueprint: AIComponentBlueprint
-@export var item_blueprint: ItemComponentBlueprint
-@export var light_blueprint: LightComponentBlueprint
-@export var inventory_blueprint: InventoryComponentBlueprint
-@export var progression_blueprint: ProgressionComponentBlueprint
-@export var equipment_blueprint: EquipmentComponentBlueprint
+@export var components: Array[Dictionary] = []
+
+func get_components() -> Array[Dictionary]:
+	var result: Array[Dictionary] = []
+	
+	# Convert each component blueprint to a dictionary with name and properties
+	for component in components:
+		if component and component.has("name") and component.has("properties"):
+			result.append({
+				"name": component.name,
+				"properties": component.properties
+			})
+	
+	return result
+
+func add_component(name: String, properties: Dictionary = {}) -> void:
+	components.append({
+		"name": name,
+		"properties": properties
+	})
